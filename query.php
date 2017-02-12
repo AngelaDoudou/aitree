c3905b083df4107cabfc0c48d744164a527bba12
mumingv <mumingv@163.com>
2017-02-12 23:28:33 +0800

    初始化项目

---
+++ query.php
@@ -0,0 +1,23 @@
+<?php
+
+//echo "I'm php script";
+
+/*
+$result = array(
+    0 => array("tag" => "科技", "score" => 91.7),
+    1 => array("tag" => "名人", "score" => 80.1),
+    2 => array("tag" => "手机", "score" => 78.6),
+    3 => array("tag" => "智能", "score" => 65.3),
+);
+ */
+
+$result = array(
+    "小米是雷军的公司",
+    "大米是木名的公司",
+    "科技的力量是无限的",
+    "人工智能是未来的方向",
+);
+
+
+echo json_encode($result);
+
