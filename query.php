5c362405c54c11e839f9f2ac24f1755d3e3f763b
mumingv <mumingv@163.com>
2017-02-15 19:01:16 +0800

    关联表单提交和后台数据获取

--- query.php
+++ query.php
@@ -1,6 +1,8 @@
 <?php
 
-//echo "I'm php script";
+function mlog($data) {
+    file_put_contents("log.txt", json_encode($data, JSON_UNESCAPED_UNICODE)."\n", FILE_APPEND);
+}
 
 /*
 $result = array(
@@ -8,16 +10,42 @@ $result = array(
     1 => array("tag" => "名人", "score" => 80.1),
     2 => array("tag" => "手机", "score" => 78.6),
     3 => array("tag" => "智能", "score" => 65.3),
+    4 => array("tag" => "测试", "score" => 35.6),
 );
+*/
+
+//mlog($_POST);
+
+/**
+ * 中文字符串转换为单个字的数组
+ * @param mixed $str 
+ * @return void
  */
+function ch2arr($str)
+{
+    $length = mb_strlen($str, 'utf-8');
+    $array = [];
+    for ($i=0; $i<$length; $i++)  
+        $array[] = mb_substr($str, $i, 1, 'utf-8');    
+    return $array;
+}  
 
-$result = array(
-    "小米是雷军的公司",
-    "大米是木名的公司",
-    "科技的力量是无限的",
-    "人工智能是未来的方向",
-);
+if (!isset($_POST['q']) || empty($_POST['q'])) {
+    $query = "请输入查询词";
+    $queryArray[] = $query;
+} else {
+    $query = $_POST['q'];
+    $queryArray = ch2arr($query);
+}
 
+$result = [];
+foreach ($queryArray as $key => $value) {
+    $result[] = array(
+        "tag" => $value,
+        "score" => rand(0, 100),
+    ); 
+}
 
+mlog($result);
 echo json_encode($result);
 
