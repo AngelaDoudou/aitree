0763c21e06818b569927ded2da801fb1dd5bac7f
AngelaDoudou <haotian_12@163.com>
2017-07-09 20:01:30 +0800

    增加thrift客户端

--- thrift_segment/php_client/query.php
+++ thrift_segment/php_client/query.php
@@ -1,20 +1,69 @@
 <?php
 
+define('THRIFT_PHP_LIB', __DIR__.'/lib/php/lib');
+define('GEN_PHP_DIR', __DIR__.'/gen-php');
+
+require_once THRIFT_PHP_LIB.'/Thrift/ClassLoader/ThriftClassLoader.php';
+require_once __DIR__ . '/gen-php/Article/ArticleService.php';
+require_once __DIR__ . '/gen-php/Article/Types.php';
+
+use Thrift\ClassLoader\ThriftClassLoader;
+
+$loader = new ThriftClassLoader();
+$loader->registerNamespace('Thrift', THRIFT_PHP_LIB);
+// register your namespace
+$loader->registerDefinition('Article', GEN_PHP_DIR);
+$loader->register();
+
+// include here the protocols and transports that you need
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Transport\TSocket;
+use Thrift\Transport\THttpClient;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Exception\TException;
+
+// Init
+$socket = new TSocket('127.0.0.1', 9090);
+$transport = new TBufferedTransport($socket, 1024, 1024);
+$protocol = new TBinaryProtocol($transport);
+$client = new \Article\ArticleServiceClient($protocol);
+// Config
+$socket->setSendTimeout(30 * 1000);
+$socket->setRecvTimeout(30 * 1000);
+// Connect
+$transport->open();
+
+#print_r($client->Segment($_REQUEST['q'],20));
+#print_r($client->Segment("一个走向辉煌的网站,小米是雷军的，雷军是谁的？",13));
+
+$result = [];
+if (!isset($_POST['q']) || empty($_POST['q'])) {
+    //$query = "请输入查询词";
+    //$queryArray[] = $query;
+    $result[] = array(
+        "tag" => "请输入查询词",
+        "score" => 55,
+    ); 
+} else {
+    $query = $_POST['q'];
+    //$queryArray = ch2arr($query);
+    $queryArray = $client->Segment($query,20);
+    foreach ($queryArray as $key => $value) {
+        $result[] = array(
+            "tag" => $value->word,
+            "score" => $value->weight*3,
+        ); 
+    }
+}
+
+// Close
+$transport->close();
+
 function mlog($data) {
     file_put_contents("log.txt", json_encode($data, JSON_UNESCAPED_UNICODE)."\n", FILE_APPEND);
 }
 
-/*
-$result = array(
-    0 => array("tag" => "科技", "score" => 91.7),
-    1 => array("tag" => "名人", "score" => 80.1),
-    2 => array("tag" => "手机", "score" => 78.6),
-    3 => array("tag" => "智能", "score" => 65.3),
-    4 => array("tag" => "测试", "score" => 35.6),
-);
-*/
 
-//mlog($_POST);
 
 /**
  * 中文字符串转换为单个字的数组
@@ -30,21 +79,7 @@ function ch2arr($str)
     return $array;
 }  
 
-if (!isset($_POST['q']) || empty($_POST['q'])) {
-    $query = "请输入查询词";
-    $queryArray[] = $query;
-} else {
-    $query = $_POST['q'];
-    $queryArray = ch2arr($query);
-}
 
-$result = [];
-foreach ($queryArray as $key => $value) {
-    $result[] = array(
-        "tag" => $value,
-        "score" => rand(1, 99),
-    ); 
-}
 
 mlog($result);
 echo json_encode($result);
